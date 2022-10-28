b6249cf51617888f77659b69ce15239a47f1c56d
Njoe00 <nick.james.joe@gmail.com>
2022-10-27 20:21:21 -0700

    updated files to have have some constant variables placed into a separate file

--- client.js
+++ client.js
@@ -1,20 +1,16 @@
 const net = require("net");
+const {IP, PORT} = require("./constants");
 
-// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
+    host: IP,
+    port: PORT
   });
-
-  // interpret incoming data as text
 
   conn.on("connect", () => {
     console.log('Connection to server established');
     conn.write("Name: WIN"),
-    setInterval (() =>  {
-    // conn.write("Move: up"),
-    }, 250);    
+   
 
     conn.on('data', data => {
       console.log(data);
---
+++ constants.js
@@ -0,0 +1,9 @@
+const PORT =  '165.227.47.243';
+const IP = 50541;
+
+
+
+module.exports = {
+    IP,
+    PORT,
+};
